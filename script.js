dce3756082820379976ed083009b95940ce1425f
Barss916 <ilya.surgutskij11@mail.ru>
2025-10-31 07:45:24 +0500

    api

--- script.js
+++ script.js
@@ -10,37 +10,6 @@ const saveNoteBtn = document.getElementById('saveNoteBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const notesList = document.getElementById("list");
 
-let notes = [{
-    "id": 1,
-    "title": "fdas",
-    "tag": 12
-},
-{
-    "id": 2,
-    "title": "fdas",
-    "tag": 12
-},
-{
-    "id": 3,
-    "title": "fdas",
-    "tag": 12
-},
-{
-    "id": 4,
-    "title": "fdas",
-    "tag": 12
-}];
-
-function getMaxId() {
-    for (let note of notes) {
-        if (note.id > maxId) {
-            maxId = note.id;
-        }
-    }
-}
-
-getMaxId();
-
 async function initData() {
    try
     {const res = await fetch("http://localhost:5000", {method : "GET"});
@@ -50,14 +19,45 @@ async function initData() {
     }
 
     const data = await res.json();
-    console.log(data);
-    return data;
+    console.log(JSON.stringify(data));
+    for(let note of data){
+        const id = note.id;
+        const title = note.title;
+        const tag_text = note.tag;
+        const date = note.date; // текущая дата и время
+        console.log(id);
+        const noteDiv = document.createElement('div');
+        noteDiv.className = 'note';
+        noteDiv.id = id;
+        noteDiv.innerHTML = `<div class="note__left__part">
+                                <span class="note__title">${title}</span>
+                                <span class="note__date">${date}</span>
+                            </div>
+                            <div class="note__tag">
+                                <span class="tag">${tag_text || '—'}</span>
+                            </div>`;
+        notesList.appendChild(noteDiv);
+    }
     } catch (e){
         console.log(e)
     }
 }
 
 initData();
+
+async function createData(id, title, tag, date) {
+   try
+    {const res = await fetch("http://localhost:5000/notes", {method : "POST", body: JSON.stringify({"id": id, "title": title, "tag": tag, "date": date})});
+    if (!res.ok){
+        console.log("404");
+        return 0;
+    }
+    const data = await res.json();
+    console.log(JSON.stringify(data));
+} catch (e){
+    console.log(e)
+}
+}
 
 newNote.addEventListener("click", () => {
     modal.style.display = 'flex';
@@ -68,14 +68,18 @@ closeModalBtn.addEventListener('click', () => {
 });
 
 saveNoteBtn.addEventListener("click", () => {
+    const id = maxId;
     const title = document.getElementById('titleInput').value.trim();
     const tag = document.getElementById('tagInput');
     const tag_text = tag.options[tag.selectedIndex].text;
     const date = new Date().toLocaleString(); // текущая дата и время
-
+    console.log(id);
     if (title) {
+        maxId++;
+        createData(id, title, tag_text, date);
         const noteDiv = document.createElement('div');
         noteDiv.className = 'note';
+        noteDiv.id = id;
         noteDiv.innerHTML = `<div class="note__left__part">
                                 <span class="note__title">${title}</span>
                                 <span class="note__date">${date}</span>
